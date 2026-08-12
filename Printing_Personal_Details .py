c548242a412c06e8ac0817c0d676907fba2a8220
Sourajit Balabantaray <sourajeetbalabantaray5@gmail.com>
2026-08-12 22:06:07 +0530

    Add personal details printing program
    This program collects and prints personal details including name, registration number, age, department, university name, and city.

---
+++ Printing_Personal_Details .py
@@ -0,0 +1,17 @@
+#program 1
+name=input("Enter your name:")
+regno=input("Enter your resiatration number:")
+age=int(input("Enter your age:"))
+dept=input("Enter your Department:")
+Uname=input("Enter your University name:")
+city=input("Enter your city name:")
+
+print(" Name is:",name)
+print(" Resiatration number:",regno)
+print(" Age:",age)
+print(" Department :",dept)
+print(" University name:",Uname)
+print(" city name:",city)
+
+
+print(f" my name is {name}. My resistration number is {regno} and i am {age} years old.I am in {Uname} and I am in {dept} Department and I this present in this {city}")
