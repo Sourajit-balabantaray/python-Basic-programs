3183f382e632d2b09de8eb5ca33845dd0bb39734
Sourajit Balabantaray <sourajeetbalabantaray5@gmail.com>
2026-08-12 22:08:04 +0530

    Add basic arithmetic operations in Python

---
+++ basic_arithmatic_operation.py
@@ -0,0 +1,19 @@
+#program 3
+# Basic Arithmatic operation
+
+a=int(input("Enter the value of first number:"))
+b=int(input("Enter the value of second number:"))
+c=a+b
+d=a-b
+e=a*b
+f=a/b
+g=a//b
+h=a%b
+i=a**b
+print(" Sum of Two numbers is:",c)
+print(" Subtraction of Two numbers is:",d)
+print(" product of Two numbers is:",e)
+print(" Quotient :",f)
+print(" Floor quotient:",g)
+print(" reminder :",h)
+print(" Power:",i)
